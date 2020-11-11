7fe54c7bc9e286cf888d747e92b4f3a37f47033d
evilevgeniys <903jenya@gmail.com>
2020-11-11 19:32:36 +0300

    added working code

--- 3.py
+++ 3.py
@@ -1,4 +1,5 @@
-# Пишем функцию проверки числа на простоту
+Рабочий, но очень медленный вариант работы программы  
+Пишем функцию проверки числа на простоту
 prime_list = []
 def if_prime_number(number):
 	d = 2
@@ -6,15 +7,12 @@ def if_prime_number(number):
 		d += 1 
 	return d * d > number
 
-#пишем функцию поиска делителей
 def search_prime_number(num):
 	for i in range(2, num):
 		if num % i == 0:
-			if if_prime_number(i):
-				prime_list.append(i)
-	return prime_list[len(prime_list) - 1]
-
-				
-
-print(search_prime_number(600851475143))
-			
+				if if_prime_number(i):
+					prime_list.append(i)
+	if not len(prime_list) == 0:
+		print (f'Самое большое простое число являющиеся делителем = {prime_list[int(len(prime_list)) - 1]}')
+	else:
+		print ('Не найдено ни одного простого числа')
